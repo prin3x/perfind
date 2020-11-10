5ed0634119f33a3e29762187c1804af478e3f6bd
Sarut Trikham <sarut1994@gmail.com>
2020-11-10 15:12:53 +0700

    [finish my part]

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {GiHamburgerMenu} from 'react-icons/gi';
 import {AiOutlineUser} from 'react-icons/ai';
 import {RiShoppingCartLine} from 'react-icons/ri';
 import {Input} from 'antd';
+import Logo from '../../assets/logo.svg';
 const {Search} = Input;
 
 const HamContainer = styled.div`
   display: inline-block;
   cursor: pointer;
   height: 12px;
+  z-index: 5000;
+  background: transparent;
 
   .bar1,
   .bar2,
   .bar3 {
     width: 24px;
+    z-index: 500;
     height: 3px;
     background-color: #8a7d6b;
     margin-top: 3px;
@@ -35,23 +39,23 @@ const HamContainer = styled.div`
 `;
 const NavbarWrapper = styled.div`
   min-height: 2rem;
-  height: 6rem;
+  height: 7rem;
   width: 100vw;
   position: relative;
   position: fixed;
   top: 0;
-  z-index: 50000;
+  z-index: 5000;
 `;
 
 const UpperNavbar = styled.div`
   min-height: 1rem;
-  height: 3rem;
+  height: 3.5rem;
   width: 100%;
   background: linear-gradient(to right, #b8956c, #e3d5b0);
 `;
 const LowerNavbar = styled.div`
   min-height: 1.5rem;
-  height: 3rem;
+  height: 3.5rem;
   width: 100%;
   background: #fff;
   display: flex;
@@ -60,7 +64,6 @@ const LowerNavbar = styled.div`
 
 const MiddleContainer = styled.div`
   min-height: 2rem;
-  height: 4.5rem;
   width: 13rem;
 
   position: absolute;
@@ -70,10 +73,10 @@ const MiddleContainer = styled.div`
 `;
 
 const ImageContainer = styled.div`
-  background: url('assets/index/logo.png') center center no-repeat #fff;
-  background-size: cover;
+  background: url('assets/index/navbar-logo-01.png') center center no-repeat;
+  background-size: contain;
   min-height: 2rem;
-  height: 4.5rem;
+  height: 7.5rem;
   width: 13rem;
 `;
 
@@ -90,28 +93,47 @@ const RightContainer = styled.div`
   display: flex;
 `;
 
-const iconFontSize = {fontSize: '2rem'};
+const Menu = styled.div`
+  height: 2rem;
+  width: 2rem;
+  border-radius: 50%;
+  position: fixed;
+  top: 3rem;
+  left: 0.25rem;
+  background: radial-gradient(#b8956c, #e3d5b0);
+  z-index: -1;
+  transition: transform 0.8s ease-in;
+  transform: scale(${props => props.scale});
+`;
+
+const iconFontSize = {fontSize: '1.75rem', color: '#8a7d6b'};
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  const handleMenu = () => {
+    setShow(!show);
+  };
   return (
     <NavbarWrapper>
+      <Menu scale={show ? '250' : '0'} />
       <UpperNavbar></UpperNavbar>
       <MiddleContainer>
-        <ImageContainer />
+        <img src={Logo} alt='' />
       </MiddleContainer>
       <LowerNavbar>
         <LeftContainer>
-          <HamContainer>
-            <div class='bar1'></div>
-            <div class='bar2'></div>
-            <div class='bar3'></div>
+          <HamContainer onClick={handleMenu}>
+            <div className={`bar1 ${show ? 'change' : ''}`}></div>
+            <div className={`bar2 ${show ? 'change' : ''}`}></div>
+            <div className={`bar3 ${show ? 'change' : ''}`}></div>
           </HamContainer>
         </LeftContainer>
         <RightContainer>
           <Search
             placeholder='input search text'
-            enterButton='Search'
-            size='large'
+            size='medium'
+            style={{borderRadius: '10px'}}
             allowClear
           />
           <RiShoppingCartLine style={iconFontSize} />
