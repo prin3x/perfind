a11c8e579ecb9cfb525d7ff2f54d8434ef2d1f86
Tantikorn Chongsamran <smallvillesv2539@gmail.com>
2020-11-24 10:51:12 +0700

    bugs login

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,37 @@
-require('dotenv').config();
-require('colors');
+require("dotenv").config();
+require("colors");
 
-const express = require('express');
-const cors = require('cors');
-const db = require('./models');
-const authRoutes = require('./routes/authRoutes');
-const productRoutes = require('./routes/productRoutes');
-const passport = require('passport');
-const logger = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const db = require("./models");
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const passport = require("passport");
+const logger = require("morgan");
 
 const server = express();
 
-require('./config/passport/passport');
-require('./config/passport/passport-facebook');
+require("./config/passport/passport");
+require("./config/passport/passport-facebook");
 
 server.use(passport.initialize());
 server.use(passport.session());
 
 server.use(cors());
-server.use(logger('tiny'));
+server.use(logger("tiny"));
 server.use(express.json());
-<<<<<<< HEAD
-server.use(express.static('public/images'));
-server.use(express.urlencoded({extended: false}));
-=======
+server.use(express.static("public/images"));
 server.use(express.urlencoded({ extended: false }));
->>>>>>> origin/contectForm
 
-server.use('/auth', authRoutes);
-server.use('/products', productRoutes);
+server.use("/auth", authRoutes);
+server.use("/products", productRoutes);
 
 db.sequelize
   .sync({ force: false })
   .then(() => {
     console.log(`DABATSE HAS BEEN SYNCING`.cyan.bold.underline);
   })
-  .catch(err =>
+  .catch((err) =>
     console.log(`There might be some err : ${err}`.red.bold.bgWhite)
   );
 
